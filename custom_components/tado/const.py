12272fca2a77140936e97529b0af6065e943bc6f
nomon0705 <luckybit0512@gmail.com>
2025-08-05 15:19:14 +0800

    refactor:const file updated
    Co-authored-by: luckybit0512 <bilaldanielsson495@gmail.com>
    Co-authored-by: nomon0705 <luckybit0512@gmail.com>
    Co-authored-by: prince777-k <b.laurent.ace@gmail.com>

--- custom_components/tado/const.py
+++ custom_components/tado/const.py
@@ -24,7 +24,8 @@ from homeassistant.components.climate import (
     HVACMode,
 )
 
-TADO_HVAC_ACTION_TO_HA_HVAC_ACTION = {
+# HVAC Action mappings
+TADO_HVAC_ACTION_TO_HA = {
     CONST_HVAC_HEAT: HVACAction.HEATING,
     CONST_HVAC_DRY: HVACAction.DRYING,
     CONST_HVAC_FAN: HVACAction.FAN,
@@ -34,12 +35,12 @@ TADO_HVAC_ACTION_TO_HA_HVAC_ACTION = {
     CONST_HVAC_HOT_WATER: HVACAction.HEATING,
 }
 
-# Configuration
+# Configuration constants
 CONF_FALLBACK = "fallback"
 CONF_HOME_ID = "home_id"
 DATA = "data"
 
-# Weather
+# Weather condition to categories mapping
 CONDITIONS_MAP = {
     "clear-night": {"NIGHT_CLEAR"},
     "cloudy": {"CLOUDY", "CLOUDY_MOSTLY", "NIGHT_CLOUDY"},
@@ -55,192 +56,169 @@ CONDITIONS_MAP = {
 }
 
 # Types
-TYPE_AIR_CONDITIONING = "AIR_CONDITIONING"
-TYPE_HEATING = "HEATING"
-TYPE_HOT_WATER = "HOT_WATER"
+class Types:
+    AIR_CONDITIONING = "AIR_CONDITIONING"
+    HEATING = "HEATING"
+    HOT_WATER = "HOT_WATER"
+    BATTERY = "BATTERY"
+    POWER = "POWER"
 
-TYPE_BATTERY = "BATTERY"
-TYPE_POWER = "POWER"
+# Modes
+class Modes:
+    OFF = "OFF"
+    SMART_SCHEDULE = "SMART_SCHEDULE"
+    AUTO = "AUTO"
+    COOL = "COOL"
+    HEAT = "HEAT"
+    DRY = "DRY"
+    FAN = "FAN"
 
-# Base modes
-CONST_MODE_OFF = "OFF"
-CONST_MODE_SMART_SCHEDULE = "SMART_SCHEDULE"  # Use the schedule
-CONST_MODE_AUTO = "AUTO"
-CONST_MODE_COOL = "COOL"
-CONST_MODE_HEAT = "HEAT"
-CONST_MODE_DRY = "DRY"
-CONST_MODE_FAN = "FAN"
+# Overlay modes
+OVERLAY_MODES = {
+    "NEXT_TIME_BLOCK": "NEXT_TIME_BLOCK",
+    "MANUAL": "MANUAL",
+    "TADO_DEFAULT": "TADO_DEFAULT",
+}
+OVERLAY_MODE_LIST = list(OVERLAY_MODES.values())
 
-CONST_LINK_OFFLINE = "OFFLINE"
+# Overlay group
+OVERLAY_GROUP = "overlay_group"
 
-CONST_FAN_OFF = "OFF"
-CONST_FAN_AUTO = "AUTO"
-CONST_FAN_LOW_LEGACY = "LOW"
-CONST_FAN_MIDDLE_LEGACY = "MIDDLE"
-CONST_FAN_HIGH_LEGACY = "HIGH"
-
-CONST_FAN_LEVEL_1 = "LEVEL1"
-CONST_FAN_LEVEL_2 = "LEVEL2"
-CONST_FAN_LEVEL_3 = "LEVEL3"
-
-
-# When we change the temperature setting, we need an overlay mode
-CONST_OVERLAY_TADO_MODE = (
-    "NEXT_TIME_BLOCK"  # wait until tado changes the mode automatic
-)
-CONST_OVERLAY_MANUAL = "MANUAL"  # the user has change the temperature or mode manually
-CONST_OVERLAY_TIMER = "TIMER"  # the temperature will be reset after a timespan
-CONST_OVERLAY_TADO_DEFAULT = (
-    "TADO_DEFAULT"  # use the setting from tado zone itself (set in Tado app or webapp)
-)
-CONST_OVERLAY_TADO_OPTIONS = [
-    CONST_OVERLAY_TADO_MODE,
-    CONST_OVERLAY_MANUAL,
-    CONST_OVERLAY_TADO_DEFAULT,
-]
-CONST_EXCLUSIVE_OVERLAY_GROUP = (
-    "overlay_group"  # Overlay group for set_climate_timer service
-)
-
-
-# Heat always comes first since we get the
-# min and max temperatures for the zone from
-# it.
-# Heat is preferred as it generally has a lower minimum temperature
+# Known modes order (priority)
 ORDERED_KNOWN_TADO_MODES = [
-    CONST_MODE_HEAT,
-    CONST_MODE_COOL,
-    CONST_MODE_AUTO,
-    CONST_MODE_DRY,
-    CONST_MODE_FAN,
+    Modes.HEAT,
+    Modes.COOL,
+    Modes.AUTO,
+    Modes.DRY,
+    Modes.FAN,
 ]
 
-TADO_MODES_TO_HA_CURRENT_HVAC_ACTION = {
-    CONST_MODE_HEAT: HVACAction.HEATING,
-    CONST_MODE_DRY: HVACAction.DRYING,
-    CONST_MODE_FAN: HVACAction.FAN,
-    CONST_MODE_COOL: HVACAction.COOLING,
+# Tado modes to HA HVAC actions
+TADO_MODE_TO_HA_ACTION = {
+    Modes.HEAT: HVACAction.HEATING,
+    Modes.DRY: HVACAction.DRYING,
+    Modes.FAN: HVACAction.FAN,
+    Modes.COOL: HVACAction.COOLING,
 }
 
-# These modes will not allow a temp to be set
-TADO_MODES_WITH_NO_TEMP_SETTING = [CONST_MODE_AUTO, CONST_MODE_FAN]
-#
-# HVAC_MODE_HEAT_COOL is mapped to CONST_MODE_AUTO
-#    This lets tado decide on a temp
-#
-# HVAC_MODE_AUTO is mapped to CONST_MODE_SMART_SCHEDULE
-#    This runs the smart schedule
-#
-HA_TO_TADO_HVAC_MODE_MAP = {
-    HVACMode.OFF: CONST_MODE_OFF,
-    HVACMode.HEAT_COOL: CONST_MODE_AUTO,
-    HVACMode.AUTO: CONST_MODE_SMART_SCHEDULE,
-    HVACMode.HEAT: CONST_MODE_HEAT,
-    HVACMode.COOL: CONST_MODE_COOL,
-    HVACMode.DRY: CONST_MODE_DRY,
-    HVACMode.FAN_ONLY: CONST_MODE_FAN,
+# Modes where temperature cannot be set
+TADO_MODES_WITH_NO_TEMP = [Modes.AUTO, Modes.FAN]
+
+# Mapping between HA and Tado modes
+HA_TO_TADO_HVAC_MODE = {
+    HVACMode.OFF: Modes.OFF,
+    HVACMode.HEAT_COOL: Modes.AUTO,
+    HVACMode.AUTO: Modes.SMART_SCHEDULE,
+    HVACMode.HEAT: Modes.HEAT,
+    HVACMode.COOL: Modes.COOL,
+    HVACMode.DRY: Modes.DRY,
+    HVACMode.FAN_ONLY: Modes.FAN,
 }
 
-HA_TO_TADO_FAN_MODE_MAP_LEGACY = {
-    FAN_AUTO: CONST_FAN_AUTO,
-    FAN_OFF: CONST_FAN_OFF,
-    FAN_LOW: CONST_FAN_LOW_LEGACY,
-    FAN_MEDIUM: CONST_FAN_MIDDLE_LEGACY,
-    FAN_HIGH: CONST_FAN_HIGH_LEGACY,
+# Legacy fan mode mappings
+FAN_MODE_LEGACY_MAP = {
+    FAN_AUTO: Modes.AUTO,
+    FAN_OFF: Modes.OFF,
+    FAN_LOW: "LOW",
+    FAN_MEDIUM: "MIDDLE",
+    FAN_HIGH: "HIGH",
 }
 
-HA_TO_TADO_FAN_MODE_MAP = {
-    FAN_AUTO: CONST_FAN_AUTO,
-    FAN_OFF: CONST_FAN_OFF,
-    FAN_LOW: CONST_FAN_LEVEL_1,
-    FAN_MEDIUM: CONST_FAN_LEVEL_2,
-    FAN_HIGH: CONST_FAN_LEVEL_3,
+# Current fan mode mappings
+FAN_MODE_MAP = {
+    FAN_AUTO: "LEVEL1",
+    FAN_OFF: "LEVEL0",
+    FAN_LOW: "LEVEL1",
+    FAN_MEDIUM: "LEVEL2",
+    FAN_HIGH: "LEVEL3",
 }
 
-TADO_TO_HA_HVAC_MODE_MAP = {
-    value: key for key, value in HA_TO_TADO_HVAC_MODE_MAP.items()
-}
+# Reverse mappings
+def invert_dict(d):
+    return {v: k for k, v in d.items()}
 
-TADO_TO_HA_FAN_MODE_MAP_LEGACY = {
-    value: key for key, value in HA_TO_TADO_FAN_MODE_MAP_LEGACY.items()
-}
+TADO_TO_HA_HVAC_MODE = invert_dict(HA_TO_TADO_HVAC_MODE)
+TADO_TO_HA_FAN_MODE_LEGACY = invert_dict(FAN_MODE_LEGACY_MAP)
+TADO_TO_HA_FAN_MODE = invert_dict(FAN_MODE_MAP)
 
-TADO_TO_HA_FAN_MODE_MAP = {value: key for key, value in HA_TO_TADO_FAN_MODE_MAP.items()}
+# Fan speeds and levels
+TADO_FAN_SPEEDS = list(FAN_MODE_LEGACY_MAP.values())
+TADO_FAN_LEVELS = list(FAN_MODE_MAP.values())
 
-TADO_FAN_SPEEDS = list(HA_TO_TADO_FAN_MODE_MAP_LEGACY.values())
-
-TADO_FAN_LEVELS = list(HA_TO_TADO_FAN_MODE_MAP.values())
-
+# Default precision
 DEFAULT_TADO_PRECISION = 0.1
 
-# Constant for Auto Geolocation mode
+# Presets
 PRESET_AUTO = "auto"
-
 SUPPORT_PRESET_AUTO = [PRESET_AWAY, PRESET_HOME, PRESET_AUTO]
 SUPPORT_PRESET_MANUAL = [PRESET_AWAY, PRESET_HOME]
 
-SENSOR_DATA_CATEGORY_WEATHER = "weather"
-SENSOR_DATA_CATEGORY_GEOFENCE = "geofence"
-
-TADO_SWING_OFF = "OFF"
-TADO_SWING_ON = "ON"
-
-HA_TO_TADO_SWING_MODE_MAP = {
-    SWING_OFF: TADO_SWING_OFF,
-    SWING_ON: TADO_SWING_ON,
+# Sensor categories
+SENSOR_DATA_CATEGORY = {
+    "WEATHER": "weather",
+    "GEOFENCE": "geofence",
 }
 
-TADO_TO_HA_SWING_MODE_MAP = {
-    value: key for key, value in HA_TO_TADO_SWING_MODE_MAP.items()
-}
+# Swing modes
+SWING_OFF = "OFF"
+SWING_ON = "ON"
 
+# Swing mode mappings
+SWING_MODE_MAP = {
+    SWING_OFF: "OFF",
+    SWING_ON: "ON",
+}
+REVERSE_SWING_MODE_MAP = invert_dict(SWING_MODE_MAP)
+
+# Domain and signals
 DOMAIN = "tado"
-
-SIGNAL_TADO_UPDATE_RECEIVED = "tado_update_received_{}_{}_{}"
-SIGNAL_TADO_MOBILE_DEVICE_UPDATE_RECEIVED = "tado_mobile_device_update_received_{}"
+SIGNAL_UPDATE_RECEIVED = "tado_update_received_{}_{}_{}"
+SIGNAL_MOBILE_UPDATE_RECEIVED = "tado_mobile_device_update_received_{}"
 UNIQUE_ID = "unique_id"
-
 DEFAULT_NAME = "Tado"
 
-TADO_HOME = "Home"
-TADO_ZONE = "Zone"
+# Home and zone
+HOME = "Home"
+ZONE = "Zone"
 
-# Constants for Temperature Offset
-INSIDE_TEMPERATURE_MEASUREMENT = "INSIDE_TEMPERATURE_MEASUREMENT"
-TEMP_OFFSET = "temperatureOffset"
-TADO_OFFSET_CELSIUS = "celsius"
-HA_OFFSET_CELSIUS = "offset_celsius"
-TADO_OFFSET_FAHRENHEIT = "fahrenheit"
-HA_OFFSET_FAHRENHEIT = "offset_fahrenheit"
-TADO_TO_HA_OFFSET_MAP = {
-    TADO_OFFSET_CELSIUS: HA_OFFSET_CELSIUS,
-    TADO_OFFSET_FAHRENHEIT: HA_OFFSET_FAHRENHEIT,
-}
+# Temperature offset constants
+class TemperatureOffset:
+    MEASUREMENT = "INSIDE_TEMPERATURE_MEASUREMENT"
+    OFFSET = "temperatureOffset"
+    TADO_CELSIUS = "celsius"
+    HA_CELSIUS = "offset_celsius"
+    TADO_FAHRENHEIT = "fahrenheit"
+    HA_FAHRENHEIT = "offset_fahrenheit"
+    OFFSET_MAP = {
+        TADO_CELSIUS: HA_CELSIUS,
+        TADO_FAHRENHEIT: HA_FAHRENHEIT,
+    }
 
-# Constants for Overlay Default settings
-HA_TERMINATION_TYPE = "default_overlay_type"
-HA_TERMINATION_DURATION = "default_overlay_seconds"
+# Overlay default settings
+class OverlayDefaults:
+    TERMINATION_TYPE = "default_overlay_type"
+    TERMINATION_DURATION = "default_overlay_seconds"
+    MIN_TEMP = 5
+    MAX_TEMP = 40
+    X_MIN_TEMP = 5
+    X_MAX_TEMP = 30
 
-TADO_DEFAULT_MIN_TEMP = 5
-TADO_DEFAULT_MAX_TEMP = 40
-
-TADO_X_DEFAULT_MIN_TEMP = 5
-TADO_X_DEFAULT_MAX_TEMP = 30
-
-# Constants for service calls
+# Service constants
 SERVICE_ADD_METER_READING = "add_meter_reading"
 CONF_CONFIG_ENTRY = "config_entry"
 CONF_READING = "reading"
 ATTR_MESSAGE = "message"
 
+# Water heater fallback
 WATER_HEATER_FALLBACK_REPAIR = "water_heater_fallback"
 
-TADO_SWING_SETTING = "swings"
-TADO_FANSPEED_SETTING = "fanSpeeds"
-
-TADO_FANLEVEL_SETTING = "fanLevel"
-TADO_VERTICAL_SWING_SETTING = "verticalSwing"
-TADO_HORIZONTAL_SWING_SETTING = "horizontalSwing"
-
-TADO_LINE_X = "is_x"
-TADO_PRE_LINE_X = "is_pre_x"
+# Tado settings
+TADO_SETTINGS = {
+    "SWINGS": "swings",
+    "FAN_SPEEDS": "fanSpeeds",
+    "FAN_LEVEL": "fanLevel",
+    "VERTICAL_SWING": "verticalSwing",
+    "HORIZONTAL_SWING": "horizontalSwing",
+    "LINE_X": "is_x",
+    "PRE_LINE_X": "is_pre_x",
+}

357614dfce166d406a447818732466a40e011991
nomon0705 <luckybit0512@gmail.com>
2025-08-07 21:29:41 +0800

    refactor:device file updated
    Co-authored-by: luckybit0512 <bilaldanielsson495@gmail.con>
    Co-authored-by: nomon0705 <luckybit0512@gmail.com>

--- custom_components/tado/device_tracker.py
+++ custom_components/tado/device_tracker.py
@@ -3,6 +3,7 @@
 from __future__ import annotations
 
 import logging
+from typing import cast
 
 from homeassistant.components.device_tracker import (
     DOMAIN as DEVICE_TRACKER_DOMAIN,
@@ -26,27 +27,27 @@ async def async_setup_entry(
     entry: TadoConfigEntry,
     async_add_entities: AddEntitiesCallback,
 ) -> None:
-    """Set up the Tado device scanner entities."""
-    _LOGGER.debug("Setting up Tado device scanner entity")
+    """Set up Tado device tracker entities."""
+    _LOGGER.debug("Setting up Tado device tracker entities")
     tado = entry.runtime_data
     tracked_devices: set[str] = set()
 
-    # Fix non-string unique_id for device trackers (can be removed in 2025.1)
+    # Temporary fix for non-string unique IDs (remove in 2025.1)
     entity_registry = er.async_get(hass)
     for device_key in tado.data["mobile_device"]:
         entity_id = entity_registry.async_get_entity_id(
             DEVICE_TRACKER_DOMAIN, DOMAIN, device_key
         )
         if entity_id:
-            entity_registry.async_update_entity(entity_id, new_unique_id=str(device_key))
+            entity_registry.async_update_entity(
+                entity_id, new_unique_id=str(device_key)
+            )
 
     def update_devices() -> None:
-        """Update tracked devices."""
-        add_tracked_entities(hass, tado, async_add_entities, tracked_devices)
+        add_new_entities(hass, tado, async_add_entities, tracked_devices)
 
     update_devices()
 
-    # Register dispatcher for device updates
     entry.async_on_unload(
         async_dispatcher_connect(
             hass,
@@ -57,31 +58,30 @@ async def async_setup_entry(
 
 
 @callback
-def add_tracked_entities(
+def add_new_entities(
     hass: HomeAssistant,
     tado: TadoConnector,
     async_add_entities: AddEntitiesCallback,
     tracked: set[str],
 ) -> None:
-    """Add new Tado device tracker entities."""
-    _LOGGER.debug("Fetching Tado devices for (newly) tracked entities")
-    new_entities: list[TadoDeviceTrackerEntity] = []
+    """Add new Tado mobile device tracker entities."""
+    new_entities = []
 
     for device_key, device in tado.data["mobile_device"].items():
         if device_key in tracked:
             continue
 
-        _LOGGER.debug(
-            "Adding Tado device %s with deviceID %s", device["name"], device_key
+        _LOGGER.debug("Adding Tado device %s (ID: %s)", device["name"], device_key)
+        new_entities.append(
+            TadoDeviceTrackerEntity(device_id=device_key, device=device, tado=tado)
         )
-        new_entities.append(TadoDeviceTrackerEntity(device_key, device["name"], tado))
         tracked.add(device_key)
 
     async_add_entities(new_entities)
 
 
 class TadoDeviceTrackerEntity(TrackerEntity):
-    """A Tado Device Tracker entity."""
+    """Tado mobile device tracker entity."""
 
     _attr_should_poll = False
     _attr_available = False
@@ -89,53 +89,20 @@ class TadoDeviceTrackerEntity(TrackerEntity):
     def __init__(
         self,
         device_id: str,
-        device_name: str,
+        device: dict,
         tado: TadoConnector,
     ) -> None:
-        """Initialize a Tado Device Tracker entity."""
-        super().__init__()
-        self._attr_unique_id = str(device_id)
+        """Initialize the Tado tracker entity."""
         self._device_id = device_id
-        self._device_name = device_name
+        self._device_name = device["name"]
         self._tado = tado
         self._active = False
-
-    @callback
-    def update_state(self) -> None:
-        """Update the device's state."""
-        device = self._tado.data["mobile_device"][self._device_id]
-
-        # Reset availability; will set to True if geoTracking is enabled
-        self._attr_available = False
-        geo_tracking_enabled = device["settings"].get("geoTrackingEnabled", False)
-        _LOGGER.debug(
-            "Tado device %s geoTrackingEnabled: %s",
-            device["name"],
-            geo_tracking_enabled,
-        )
-
-        if not geo_tracking_enabled:
-            return
-
-        self._attr_available = True
-        # Determine if device is at home
-        location = device.get("location")
-        if location and location.get("atHome"):
-            _LOGGER.debug("Tado device %s is at home", device["name"])
-            self._active = True
-        else:
-            _LOGGER.debug("Tado device %s is not at home", device["name"])
-            self._active = False
-
-    @callback
-    def on_demand_update(self) -> None:
-        """Handle on-demand state update."""
-        self.update_state()
-        self.async_write_ha_state()
+        self._attr_unique_id = str(device_id)
 
     async def async_added_to_hass(self) -> None:
-        """Register update callback on addition."""
-        _LOGGER.debug("Registering Tado device tracker entity: %s", self._device_name)
+        """Register update callback when added to Home Assistant."""
+        _LOGGER.debug("Registered Tado tracker: %s", self._device_name)
+
         self.async_on_remove(
             async_dispatcher_connect(
                 self.hass,
@@ -145,12 +112,33 @@ class TadoDeviceTrackerEntity(TrackerEntity):
         )
         self.update_state()
 
+    @callback
+    def update_state(self) -> None:
+        """Update the internal state from Tado data."""
+        device = self._tado.data["mobile_device"][self._device_id]
+        self._attr_available = False
+
+        if device["settings"].get("geoTrackingEnabled"):
+            self._attr_available = True
+            self._active = device.get("location", {}).get("atHome", False)
+            _LOGGER.debug(
+                "Tado device %s is %s",
+                device["name"],
+                "at home" if self._active else "not at home",
+            )
+        else:
+            _LOGGER.debug("Tado device %s has geoTracking disabled", device["name"])
+
+    @callback
+    def on_demand_update(self) -> None:
+        """Trigger a state update."""
+        self.update_state()
+        self.async_write_ha_state()
+
     @property
     def name(self) -> str:
-        """Return the device name."""
         return self._device_name
 
     @property
     def location_name(self) -> str:
-        """Return the current location state."""
         return STATE_HOME if self._active else STATE_NOT_HOME
